91cae9f862cb21a30dab48881447686016636416
muhammadaliteacher <muhammadalishuhratjonov50@gmail.com>
2025-02-19 16:43:18 +0500

    added new changes

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! from XN_1';
+    return 'Hello World! from Uzbekistan';
   }
 }
